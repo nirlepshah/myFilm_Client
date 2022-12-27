17318d23e2d626e66819f61981f77d51f54ea404
nirlepshah <nirlepshah@gmail.com>
2022-12-27 16:47:21 -0500

    commit

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import { Card, Form, Button, Container } from "react-bootstrap";
 
 export const ProfileView = () => {
   let storedUser = JSON.parse(localStorage.getItem("user"));
@@ -61,73 +61,108 @@ export const ProfileView = () => {
     }
   };
 
+  const handleDelete = (e) => {
+    e.preventDefault();
+    if (confirm("Are you sure? This cannot be undone!")) {
+      fetch(`https://myfilm-api.onrender.com/users/${storedUser.Username}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${storedToken}`,
+        },
+      })
+        .then((res) => {
+          alert(`Your account has been deleted. We're sorry to see you go!`);
+          localStorage.clear();
+          window.open("/", "_self");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
   return (
     <>
-      <Card className="mt-5">
-        <Card.Body>
-          <Card.Title>User Details</Card.Title>
-          <Card.Text>Username: {storedUser.Username}</Card.Text>
-          <Card.Text>Email: {storedUser.Email}</Card.Text>
+      <Container>
+        <Card className="mt-5">
+          <Card.Body>
+            <Card.Title>User Details</Card.Title>
+            <Card.Text>Username: {storedUser.Username}</Card.Text>
+            <Card.Text>Email: {storedUser.Email}</Card.Text>
 
-          <Card.Text>
-            {storedUser.Birthday
-              ? `Birthday: ${storedUser.Birthday}`
-              : `Birthday: You did not provide your Birthday.`}
-          </Card.Text>
-          <Card.Text>
-            {storedUser.FavoriteMovies.length === 0
-              ? `Favorite Movies: You don't have any Favorite Movies.`
-              : `Favorite Movies: ${storedUser.FavoriteMovies}`}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <h2>Please enter information below to update the profile </h2>
-      <Form onSubmit={handleUpdate}>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="text"
-            required
-            minlength="3"
-            placeholder="Enter Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email:</Form.Label>
-          <Form.Control
-            type="email"
-            required
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formBirthday">
-          <Form.Label>Birthday:</Form.Label>
-          <Form.Control
-            type="date"
-            required
-            minlength="3"
-            placeholder="Enter Birthday"
-            value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="text"
-            required
-            minlength="3"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button type="submit">Submit</Button>
-      </Form>
+            <Card.Text>
+              {storedUser.Birthday
+                ? `Birthday: ${storedUser.Birthday}`
+                : `Birthday: You did not provide your Birthday.`}
+            </Card.Text>
+            <Card.Text>
+              {storedUser.FavoriteMovies.length === 0
+                ? `Favorite Movies: You don't have any Favorite Movies.`
+                : `Favorite Movies: ${storedUser.FavoriteMovies}`}
+            </Card.Text>
+
+            <h2>Please enter information below to update the profile </h2>
+            <Form onSubmit={handleUpdate}>
+              <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                  type="text"
+                  required
+                  minlength="3"
+                  placeholder="Enter Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  required
+                  placeholder="Enter Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control
+                  type="date"
+                  required
+                  minlength="3"
+                  placeholder="Enter Birthday"
+                  value={birthday}
+                  onChange={(e) => setBirthday(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                  type="text"
+                  required
+                  minlength="3"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <br />
+              <Button type="submit">Submit</Button>
+            </Form>
+            <br />
+            <br />
+            <Card>
+              <h2>Click the button below to Delete Your Account </h2>
+              <Card.Body>
+                <Button variant="danger" type="submit" onClick={handleDelete}>
+                  DELETE ACCOUNT PERMANENTLY
+                </Button>
+              </Card.Body>
+            </Card>
+          </Card.Body>
+        </Card>
+      </Container>
     </>
   );
 };
